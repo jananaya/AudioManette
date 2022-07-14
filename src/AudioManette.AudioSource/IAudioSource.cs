e381d11226322b98d0adb7f4f28ed42a8da196a6
jananaya <jancarlosanayajimenez@gmail.com>
2022-07-14 17:27:57 -0500

    Define interface for the AudioSource classes

---
+++ src/AudioManette.AudioSource/IAudioSource.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AudioManette.AudioSource
+{
+    public interface IAudioSource
+    {
+        Task<List<Track>> SearchTrack(string name);
+        Task<bool> PlayTrack(string uri);
+        Task<bool> PausePlayback();
+        Task<bool> ResumePlayback();
+        Task<bool> SetVolume(int volumePercent); 
+        Task<bool> EnableRepeatMode();
+        Task<bool> DisableRepeatMode();
+        Task<Track> GetCurrentTrack();
+    }
+}
