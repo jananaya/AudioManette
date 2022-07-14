e3aced12244e2162330513b73a1a3be8b6f6533b
jananaya <jancarlosanayajimenez@gmail.com>
2022-07-14 17:35:32 -0500

    Write mapper class for create domain objects from API objects

---
+++ src/SpotifyAudioSource/SpotifyAudioSourceMapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SpotifyAPI.Web;
+
+namespace AudioManette.AudioSource
+{
+    internal class SpotifyAudioSourceMapper
+    {
+        public static Track ToTrack(FullTrack fulltrack)
+        {
+            return new Track(
+                    fulltrack.Uri,
+                    fulltrack.Name,
+                    fulltrack.Artists.First().Name,
+                    (
+                        Little: FindImageUrlByWidth(fulltrack.Album.Images, 64),
+                        Medium: FindImageUrlByWidth(fulltrack.Album.Images, 300)
+                    ),
+                    fulltrack.DurationMs
+                );
+        }
+
+        public static List<Track> ToTrack(List<FullTrack> fullTracks)
+        {
+            List<Track> tracks = new List<Track>();
+
+            foreach (var fulltrack in fullTracks)
+            {
+                tracks.Add(SpotifyAudioSourceMapper.ToTrack(fulltrack));
+            }
+
+            return tracks;
+        }
+
+        private static string FindImageUrlByWidth(List<Image> images, int width)
+        {
+            Image? image = images.Find(e => e.Width == width);
+
+            return image?.Url ?? "";
+        }
+    }
+}
