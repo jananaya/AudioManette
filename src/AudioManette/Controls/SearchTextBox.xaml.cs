0952c71dd4d5c53a16e5d63ea9a40a033ebf0e12
Jan Carlos Anaya Jiménez <jancarlosanayajimenez@gmail.com>
2022-07-27 18:08:18 -0500

    AudioManette GUI (#2)
    * Write SpotifyAudioSource class
    
    * Replace method to get current track for method to get current playing state
    
    * Remove MVVM folder structure
    
    * Create custom TextBox control for search tracks
    
    * Create custom control for present track data
    
    * Add reference to the others projects in MSBuild config file
    
    * Write Util class for use in the views
    
    * Write initial code for the Player window
    
    * Add reference to configuration file
    
    * Create application entry point
    
    * Give functionality to the volume knob

---
+++ src/AudioManette/Controls/SearchTextBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+using AudioManette.AudioSource;
+
+namespace AudioManette.Controls
+{
+    /// <summary>
+    /// Interaction logic for SearchTextBox.xaml
+    /// </summary>
+    public partial class SearchTextBox : UserControl
+    {
+        private IAudioSource _audioSource;
+        
+        public delegate void SearchTrackEventHandler(object sender);
+        public event SearchTrackEventHandler? searchTrackEvent;
+
+        public SearchTextBox(IAudioSource audioSource)
+        {
+            _audioSource = audioSource;
+            
+            InitializeComponent();
+        }
+        
+        
+        private async void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (searchTrackEvent == null)
+            {
+                return;
+            }
+
+            var textbox = (TextBox)sender;
+
+            if (textbox.Text == "")
+            {
+                return;
+            }
+
+            List<Track> tracks = await _audioSource.SearchTrack(textbox.Text);
+
+            searchTrackEvent(tracks);
+        }
+
+        private void ClearSearchInputBtn_Click(object sender, RoutedEventArgs e)
+        {
+            searchTextbox.Text = "";
+        }
+    }
+}
