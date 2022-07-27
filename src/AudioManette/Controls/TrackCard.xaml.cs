7416d723288c1681595a6b2912c1bdd5789f4542
jananaya <jancarlosanayajimenez@gmail.com>
2022-07-27 09:07:32 -0500

    Create custom control for present track data

---
+++ src/AudioManette/Controls/TrackCard.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using AudioManette.AudioSource;
+
+namespace AudioManette.Controls
+{
+    /// <summary>
+    /// Interaction logic for TrackCard.xaml
+    /// </summary>
+    public partial class TrackCard : UserControl
+    {
+        private IAudioSource _audioSource;
+        private Track _track;
+        
+        public event TrackChangedEventHandler? trackChangedEvent;
+        public delegate void TrackChangedEventHandler(object sender);
+        
+        public TrackCard(IAudioSource audioSource, Track track)
+        {
+            _audioSource = audioSource;
+            _track = track;
+
+            var duration = new TimeSpan(0, 0, 0, 0, (int)track.DurationMs);
+            var durationString = duration.ToString(@"mm\:ss");
+
+            this.DataContext = new
+            {
+                ImagePath = track.ImagePath.Medium,
+                Title = track.Title,
+                Artist = track.Artist,
+                Duration = durationString
+            };
+
+            InitializeComponent();
+        }
+
+        private async void TrackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (trackChangedEvent != null)
+            {
+                await _audioSource.PlayTrack(_track.Uri);
+                trackChangedEvent(_track);
+            }
+        }
+    }
+}
