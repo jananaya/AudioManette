6432182c12d12e63afd9d0c807035aebb6aa3070
jananaya <jancarlosanayajimenez@gmail.com>
2022-07-27 08:03:48 -0500

    Replace method to get current track for method to get current playing state

---
+++ src/AudioManette.AudioSource/CurrentPlayingState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AudioManette.AudioSource
+{
+    public class CurrentPlayingState
+    {
+        public CurrentPlayingState(bool isPlaying, bool isRepeatModeEnabled,
+            float volumePercentage, Track track)
+        {
+            IsPlaying = isPlaying;
+            IsRepeatModeEnabled = isRepeatModeEnabled;
+            VolumePercentage = volumePercentage;
+            Track = track;
+        }
+
+        public bool IsPlaying { get; set; }
+        public bool IsRepeatModeEnabled { get; set; }
+        public float VolumePercentage { get; set; }
+        public Track Track { get; set; }
+    }
+}
